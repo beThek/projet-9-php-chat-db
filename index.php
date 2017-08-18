ec1cda7a41f3bad8f8cbc81b47c285017e68b29d
bethek <kreshnikiberdemaj@gmail.com>
2017-08-18 15:40:23 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>minichat2_db</title>
+    </head>
+    <body>
+        <form action="" method="post">
+            <fieldset>
+            	<p>Créer un compte</p>
+            	Pseudo : <input type="text" name="pseudo_register">
+            	Mot de passe : <input type="password" name="password_register">
+            	<button type="submit" name="register">S'enregistrer</button>
+            </fieldset>
+            </form>
+
+
+
+            <hr>
+
+            <form method="post" action="">
+            <fieldset>
+            	<p>Log in</p>
+            	Pseudo : <input type="text" name="pseudo_login">
+            	Mot de passe : <input type="password" name="password_login">
+            	<button type="submit" name="login">Log In</button>
+            </fieldset>
+</form>
+    </body>
+</html>
